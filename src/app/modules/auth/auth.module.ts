44e1fc9215f3f6146d4d1d1aa2b205db6dec0a3b
Nagaraj <nagaraju@rhbussolutions.com>
2022-11-10 09:53:19 +0530

    tickets raise branch for metronics and infragistics

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -4,11 +4,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
-import { RegistrationComponent } from './components/registration/registration.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
-import { LogoutComponent } from './components/logout/logout.component';
 import { AuthComponent } from './auth.component';
-import { TranslationModule } from '../i18n/translation.module';
 import { IgxGridModule, IgxCheckboxModule, IgxButtonModule,
   IgxSwitchModule, IgxSliderModule, IgxExcelExporterService } from "igniteui-angular";
 import { LocalDataService } from './components/login/localData.service';
@@ -16,14 +12,10 @@ import { LocalDataService } from './components/login/localData.service';
 @NgModule({
   declarations: [
     LoginComponent,
-    RegistrationComponent,
-    ForgotPasswordComponent,
-    LogoutComponent,
     AuthComponent,
   ],
   imports: [
     CommonModule,
-    TranslationModule,
     AuthRoutingModule,
     FormsModule,
     ReactiveFormsModule,
